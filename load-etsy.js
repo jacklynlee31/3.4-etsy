e4c8b62d45e44da40771f8e0a8dea71f0a3defac
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-07-18 17:01:48 -0500

    Attempt 1

---
+++ load-etsy.js
@@ -0,0 +1,19 @@
+var loadEtsy = function (query, onload, immediate) {
+    var oReq = new XMLHttpRequest();
+    var apiKey = 'ki0carhy6hn0ldkouxbju1wq';
+    var url = 'https://api.etsy.com/v2/listings/active.js?api_key=' +
+        apiKey +
+        '&keywords=' +
+        query +
+        '&includes=Images,Shop&sort_on=score';
+
+    $.ajax({
+        url: url,
+        jsonp: 'callback',
+        dataType: 'jsonp',
+        success: onload
+    });
+    if (immediate) {
+    	immediate();
+    }
+};
